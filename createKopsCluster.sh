f05c2858830373e585f3862f3f7dd8a13401abb8
w3s7y <ben.west.500@gmail.com>
2018-06-08 08:21:38 +0100

    Deploy config for isd api

--- createKopsCluster.sh
+++ createKopsCluster.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 # Generate an ssh keypair for the cluster
-ssh-keygen -t rsa -b 4096 -P '' -f k8s_id_rsa
+echo "Generating ssh keypair for cluster, please give a pass phrase."
+# ssh-keygen -t rsa -b 4096 -f k8s_id_rsa
 
 # Create a "baby" cluster.
-kops create cluster --name=k8s-westys-net --state=k8s-westys-net --zones="eu-west-1a,eu-west-1b,eu-west-1c" \
---node-count=1 --master-count=1 --node-size=t2.micro --master-size=t2.micro \
---dns-zone=westys.net --yes
+kops create cluster --name=k8s.westys.net --state=s3://k8s.westys.net --zones="eu-west-1a,eu-west-1b,eu-west-1c" \
+--node-count=1 --master-count=1 --node-size=t2.small --master-size=t2.micro \
+--ssh-public-key=k8s_id_rsa.pub --dns-zone=westys.net --yes
 
