e3d5326eda83462c3beec86d59064cc0646e076f
w3s7y <ben.west.500@gmail.com>
2018-06-14 01:53:22 +0100

    Adding resource limits to isd pods, update to version of app, edits to cluster

--- createKopsCluster.sh
+++ createKopsCluster.sh
@@ -2,10 +2,12 @@
 
 # Generate an ssh keypair for the cluster
 echo "Generating ssh keypair for cluster, please give a pass phrase."
-# ssh-keygen -t rsa -b 4096 -f k8s_id_rsa
+ssh-keygen -t rsa -b 4096 -f ${HOME}/.ssh/k8s_id_rsa
 
 # Create a "baby" cluster.
-kops create cluster --name=k8s.westys.net --state=s3://k8s.westys.net --zones="eu-west-1a,eu-west-1b,eu-west-1c" \
+kops create cluster \
+--name=k8s.westys.net --state=s3://k8s.westys.net --dns-zone=westys.net \
+--zones="eu-west-1a,eu-west-1b,eu-west-1c" \
 --node-count=1 --master-count=1 --node-size=t2.small --master-size=t2.micro \
---ssh-public-key=k8s_id_rsa.pub --dns-zone=westys.net --yes
+--ssh-public-key=${HOME}/.ssh/k8s_id_rsa.pub --yes
 
